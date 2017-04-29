b2fbc405f12c5a7766723baeee0d36c88b1ad160
YGYOOO <594487125@qq.com>
2017-04-29 14:36:04 -0500

    cur tar

--- Agent-Patrolling-Visualization/public/javascripts/react/Graph.js
+++ Agent-Patrolling-Visualization/public/javascripts/react/Graph.js
@@ -17,15 +17,30 @@ class Graph extends React.Component {
         curStep = this.props.curStep > steps ? steps : this.props.curStep,
         curRegion = this.props.curRegion,
         algorithm = this.props.algorithm;
-        
+
+    let agentsIds = [];
+    if (curRegion > -1) {
+      this.props.agents.forEach((agent, index) => {
+        let isInRegion = this.props.regions.get(curRegion).some((square) => {
+          return square.row === agent.row && square.column === agent.column;
+        });
+        if (isInRegion) agentsIds.push(agent.id.toString());
+      })
+    }
+
+    let currrentTar = {}
+    Object.keys(targets).forEach((key) => {
+      if (agentsIds.indexOf(key) > -1) currrentTar[key] = targets[key];
+    });
+
     const targetList = 
     this.props.historyTargetLists 
     && curRegion  > -1 
     && this.props.historyTargetLists[curRegion][curStep]? (
       this.props.historyTargetLists[curRegion][curStep].filter((tar) => {
         if (algorithm == 3 || algorithm == 4) return true;
-        return  Object.keys(targets).every((key) => {
-          return targets[key][curStep].row != tar.row || targets[key][curStep].column != tar.column;
+        return  Object.keys(currrentTar).every((key) => {
+          return currrentTar[key][curStep].row != tar.row || currrentTar[key][curStep].column != tar.column;
         });
       }).map((target, index) => {
         return (
@@ -36,18 +51,8 @@ class Graph extends React.Component {
       })
     ) : null;
     
-    let agentsIds = [];
-    if (curRegion > -1) {
-      this.props.agents.forEach((agent, index) => {
-        let isInRegion = this.props.regions.get(curRegion).some((square) => {
-          return square.row === agent.row && square.column === agent.column;
-        });
-        if (isInRegion) agentsIds.push(index);
-      })
-    }
-
-    const curTargets = targets ? Object.keys(targets).map((key, index) => {
-      return agentsIds.indexOf(index) > -1 && targets[key][curStep] ? (
+    const curTargets = Object.keys(currrentTar).length > 0 ? Object.keys(currrentTar).map((key, index) => {
+      return currrentTar[key][curStep] ? (
         <div key={index}>
           <div className="curTargets-agent">
             {'Agent ' + (index + 1)}
